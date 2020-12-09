2f6eeb158fc374f05dcd3fce35063ecc32781dc5
iomegak12 <jd.ramkumar@gmail.com>
2020-12-09 08:48:05 +0530

    changes

--- config/env-loader.ts
+++ config/env-loader.ts
@@ -1,7 +1,14 @@
 import path from 'path';
 import dotenv from "dotenv";
+import { LogManager } from '../common';
 
 const loadEnvironment = (environment: string) => {
+    LogManager.info("Loading the Configuration File ... and Adding to the Environment ...");
+
+    const fileName = `${process.cwd()}/environments/${environment}.env`;
+
+    LogManager.info("Configuration File ... " + fileName);
+
     dotenv.config({
         path: path.resolve(`${process.cwd()}/environments/${environment}.env`)
     });
